52c528121deabdc3b2c87a07019bfeff69725c8b
noahswift2015 <124508585+noahswift2015@users.noreply.github.com>
2026-09-08 16:59:49 +0100

    Update PlutoniumDebug.java

--- src/main/java/dev/plutoniumdebug/PlutoniumDebug.java
+++ src/main/java/dev/plutoniumdebug/PlutoniumDebug.java
@@ -13,6 +13,7 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.item.Items;
 import dev.plutoniumdebug.modules.AutoRelog;
+
 public final class PlutoniumDebug extends MeteorAddon {
     public static final Category CATEGORY = new Category(
         "Plutonium Debug",
@@ -26,6 +27,7 @@ public final class PlutoniumDebug extends MeteorAddon {
 
     @Override
     public void onInitialize() {
+        Modules.get().add(new AutoRelog());
         Modules.get().add(new LoadedChunkFinder());
         Modules.get().add(new AdvancedBlockEsp());
         Modules.get().add(new AmethystEsp());
